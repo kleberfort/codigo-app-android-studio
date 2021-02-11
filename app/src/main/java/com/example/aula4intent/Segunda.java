f8023272314cf4a8890f4fa12dcc3206350fbfda
kleberfort <kleberfort@gmail.com>
2021-02-11 10:33:13 -0300

    Start commit

---
+++ app/src/main/java/com/example/aula4intent/Segunda.java
@@ -0,0 +1,28 @@
+package com.example.aula4intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Segunda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+
+        Button button1 = findViewById(R.id.button01);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
